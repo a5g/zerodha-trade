1fc571fd3c51c11c15adbd6134ac1742c4c0cf7a
Anand Kumar Ganesh <gani.anand@gmail.com>
2025-09-05 11:54:57 +0530

    Default kcid set to 1

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ export const ACTIVE_USER = 1
 export default {
   baseURL: 'https://kite.zerodha.com',
   apiHost: 'https://kite.zerodha.com',
-  kcid: parseInt(process.env.kcid, 10) || 3,
+  kcid: parseInt(process.env.kcid, 10) || 1,
   holdingsOrder: process.env.holdingsOrder || 'net',
   users,
 }
